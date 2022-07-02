e00821d47e5e3608677e2895e2c1cbb7a645fcc8
louis2tch <louis2tch@gmail.com>
2022-07-02 09:56:15 -0400

    first update after adjusted files to donut clisker

---
+++ donut-clicker-app.js
@@ -0,0 +1,11 @@
+import Donut from "./donut-clicker";
+
+renderPage();
+
+function renderPage(){
+    runDonut();
+}
+
+function runDonut(){
+    console.log("Nous sommes ici");
+}
---
+++ donut-clicker.js
@@ -0,0 +1,11 @@
+class Donut{
+    construrtor(bakerName, numberOfClicks, numberOfAutoClickers, numberOfDunuts){
+        this.bakerName = bakerName;
+        this.numberOfClicks = numberOfClicks;
+        this.numberOfAutoClickers = numberOfAutoClickers;
+        this.numberOfDunuts = numberOfDunuts
+   }
+
+
+}
+export default Donut;
